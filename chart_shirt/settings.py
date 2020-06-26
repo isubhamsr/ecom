f5c454502573503fdad600eabc145e0c1db59abc
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2020-06-26 20:22:18 +0530

    made some changes

--- chart_shirt/settings.py
+++ chart_shirt/settings.py
@@ -123,7 +123,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
